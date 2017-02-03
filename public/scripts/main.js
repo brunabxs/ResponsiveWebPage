3fbfd5247a7908259c3e723b92fe74e815f737a0
Bruna Xavier <brunabxsantos@gmail.com>
2017-02-03 19:18:54 -0200

    Adding the search functionality.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
     $('.lm-access').tabs();
+
+    $('#lm-search button').click( function () {
+        $('#lm-search input').show();
+    });
+
+    $('#lm-search input').autocomplete({
+        source: function (request, response) {
+            jQuery.get('http://www.lovemondays.com.br/pesquisa/empresa/autocomplete', {
+                q: request.term
+            }, function (data) {
+                autocompleteData = [];
+                for (var i = 0; i < data.results.length; ++i) {
+                    autocompleteData.push(data.results[i].name);
+                }
+                response(autocompleteData);
+            });
+        },
+        crossDomain: true,
+        dataType: 'jsonp',
+        minLength: 2
+    });
 });
